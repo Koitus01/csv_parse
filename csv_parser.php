07716f1acca8cc2a2465d1ea9fd54d765dded8d0
Артём Быков <a.bykov@sprinthost.ru>
2023-04-24 18:57:47 +0300

    changing foreach + loop solution

--- csv_parser.php
+++ csv_parser.php
@@ -1,95 +1,35 @@
 <?php
 
-class Tree
-{
-    private array $items = [];
-
-    public function getItem(int $id): ?TreeItem
-    {
-        return $this->items[$id]??null;
-    }
-
-    public function setItem(TreeItem $item, int $id): void
-    {
-        $this->items[$id] = $item;
-    }
-
-    public function getRootItems(): array
-    {
-        $rootItems = [];
-        foreach ($this->items as $item) {
-            if ($item->getParent() === null)
-                $rootItems[] = $item;
-        }
-
-        return $rootItems;
-    }
-}
-
-class TreeItem
-{
-    private ?TreeItem $parent = null;
-    private array $children = [];
-    private array $data = [];
-
-    public function setParent(?TreeItem $parent): void
-    {
-        $this->parent = $parent;
-    }
-
-    public function addChild(TreeItem $child): void
-    {
-        $this->children[] = $child;
-    }
-
-    public function setData(array $data): void
-    {
-        $this->data = $data;
-    }
-}
-
 // Берем заголовки и удаляем их из массива
 $headers = str_getcsv(fgets(fopen('products.csv', 'r')));
 $file = file('products.csv');
 array_shift($file);
 // Формируем список продуктов
-$list = [];
-array_map(function ($str) use (&$list, $headers) {
+$flatProducts = [];
+array_map(function ($str) use (&$flatProducts, $headers) {
     $str = array_combine($headers, str_getcsv($str));
-    $list[$str['id']] = array_combine($headers, $str);
+    $flatProducts[$str['id']] = array_combine($headers, $str);
 }, $file);
 
-$tree = new Tree();
-foreach($list as $itemData) {
-
-    $id = $itemData['id'];
-    $pid = $itemData['parent_id'];
-
-    // if ZERO we have root element no parent exists
-
-    $parent = $tree->getItem($pid);
-    if ($pid > 0 && $parent === null) {
-        $parent = new TreeItem();
-        $tree->setItem($parent, $pid);
-    }
-
-    // create new tree item if not exists / otherwise get existing item
-    $item = $tree->getItem($id);
-    if ($item === null) {
-        $item = new TreeItem();
-        $item->setParent($parent);
-        $tree->setItem($item, $id);
-    }
-
-    if ($parent !== null) {
-        $parent->addChild($item);
-    }
-
-    $item->setData($itemData);
+// Группируем продукты по parent_id
+$groupedProducts = [];
+foreach ($flatProducts as $product) {
+    $groupedProducts[$product['parent_id']][$product['id']] = $product;
 }
 
-var_dump((array)$tree->getItem(1), $item);
-die;
-echo $item;
-echo(json_encode($tree, JSON_UNESCAPED_UNICODE));
+$productsTree = [];
+foreach ($flatProducts as &$product) {
+    $parentId = $product['parent_id'];
+    $level = 0;
+    while ($flatProducts[$parentId] ?? null) {
+        if (isset($productsTree[$parentId])) {
+            unset($productsTree[$parentId]);
+        }
+        $productsTree[$parentId]['name'] = $flatProducts[$parentId]['name'];
+        $productsTree[$parentId]['children'][$product['id']] = $product;
+        $parentId = $flatProducts[$parentId]['parent_id'];
+    }
+}
+
+echo(json_encode($productsTree, JSON_UNESCAPED_UNICODE));
 die;
